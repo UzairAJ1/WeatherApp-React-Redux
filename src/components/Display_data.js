144982b3c7f43d2128934abfce8fe1d8f2f0fc8a
uzairaj1 <uzair.aaj@gmail.com>
2023-11-16 01:57:11 -0800

    maps integration

--- src/components/Display_data.js
+++ src/components/Display_data.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { useSelector, useDispatch } from "react-redux";
+import { MapContainer, TileLayer, Marker,Popup } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+import MarkerImg from "../assets/512px-Map_marker.svg.png"
+
 const Display_data = () => {
   const dispatch = useDispatch();
   const weatherData = useSelector((state) => state.weather)
-
+console.log("Coords:",weatherData)
+console.log("lat:",weatherData.data.coord.lon)
+  const customIcon = new L.Icon({
+    iconUrl: MarkerImg, 
+    iconSize: [50, 60], 
+    iconAnchor: [10, 10],
+    popupAnchor: [0, -10],
+  });
   return (
     <>
-      <div className='text-white mt-6'>
+      <div className='text-white mt-6 w-full'>
         <h1 className='text-xl font-bold'>Weather Data</h1>
         <div >Location : {weatherData.data.name}</div>
         <div >ID : {weatherData.data.id}</div>
@@ -25,6 +37,19 @@ const Display_data = () => {
         ))}
 
 
+<div className='w-full h-[600px]'>
+    <MapContainer style={{ height: 500 }} center={[weatherData.data.coord.lat,weatherData.data.coord.lon]} zoom={7} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+    <Marker position={[weatherData.data.coord.lat,weatherData.data.coord.lon]} icon={customIcon} >
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+    </div>
       </div></>
   )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,6 +5,9 @@ import { getAllWeather, addCity } from '../features/weather/weatherSlice';
 import getLocation from '../functions/getLocation';
 import Display_data from './Display_data';
 import Error from './Error';
+
+
+
 const Hero = () => {
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
@@ -37,17 +40,25 @@ const Hero = () => {
 
   }
   const handleSelected = () => {
-    dispatch(addCity(city));
-    dispatch(getAllWeather());
-    setRedux(true);
+    if(city)
+    {
+      dispatch(addCity(city));
+      dispatch(getAllWeather());
+      setRedux(true);
+    }
+    else{
+      
+    }
+   
   }
 
   return (
     <>
+ 
 
-      <div className='w-full h-screen bg-[#1C2257] flex flex-col items-center justify-center'>
+      <div className={`w-full ${redux?"h-[2000px]":"h-screen"}  bg-[#1C2257] flex flex-col gap-6 items-center justify-center`}>
         <h1 className='text-white text-xl font-bold'>My Weather app</h1>
-        <div className='w-5/6 h-5/6 bg-[#061543] rounded-3xl'>
+        <div className='w-5/6 h-5/6 bg-[#061543] rounded-3xl flex flex-col items-center gap-6'>
           {/* <input onChange={(e)=>setCity(e.target.value)}/> */}
           <h1 className='text-white mt-12'>Get Your Location's Weather  Data</h1>
           {location === null &&
@@ -67,7 +78,7 @@ const Hero = () => {
       ))} */}
           {weatherData?.data?.message==="city not found"?( <h1 className='text-red-500 mt-12'>Enter valid city name</h1>):(redux && <Display_data />)}
      {/* {redux &&  <Display_data /> } */}
-          <div className='mt-10 text-white flex flex-col items-center justify-center gap-5'>
+          <div className=' text-white flex flex-col items-center justify-center gap-5'>
             <h1>Or Search any City or Country</h1>
             <input className="text-black" onChange={(e) => { setCity(e.target.value) }} />
             <button onClick={handleSelected} className='bg-black text-white w-20'>Submit</button>
